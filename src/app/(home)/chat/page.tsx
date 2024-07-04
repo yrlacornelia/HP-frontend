4e560ee6885281cede75bbc1dc1f9f351c52c95b
Yrla Hackzell <yrla.hackzell@hotmail.com>
2024-07-04 14:06:45 +0200

    house chat layout

--- src/app/(home)/chat/page.tsx
+++ src/app/(home)/chat/page.tsx
@@ -1,5 +1,20 @@
+import ChatSidebar from "@/components/navigation/chatSidebar";
+
 const Chat = () => {
-    return ( <><p>chat</p></> );
+
+
+
+        return ( <div className="flex"><ChatSidebar/>
+
+        <div className="flex flex-col ml-auto mr-auto  "> 
+            <div className=" border border-lg boder-black">hello</div>
+            <div>chat </div>
+        </div>
+        
+        <div className=""> 
+            right sidebar
+        </div>
+            </div> );
 }
  
 export default Chat;
---
+++ src/app/(home)/chat/private/page.tsx
@@ -0,0 +1,9 @@
+import ChatSidebar from "@/components/navigation/chatSidebar";
+
+const PrivateChat = () => {
+    return ( <div className="flex"><ChatSidebar/> private</div>
+
+    );
+}
+ 
+export default PrivateChat;
